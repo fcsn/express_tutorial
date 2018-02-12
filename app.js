066c270d71e41c50f14d3ee2f959cc2487bea669
kitchenevan <sirfcsn@gmail.com>
2018-02-12 21:04:57 +0900

    basic form of web app using express add form feat for submitting user text

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+app.use(bodyParser.urlencoded({ extended:false }));
+app.locals.pretty = true;
+app.set('views', './views');
+app.set('view engine', 'jade');
+app.get('/topic/new', function(req, res) {
+  res.render('new');
+})
+app.post('/topic', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send('HI post, '+req.body.title);
+})
+app.listen(3000, function() {
+  console.log('Connected 3000 Port!');
+})
